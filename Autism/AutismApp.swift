a8bd1d4f050891bbad0d9c478009f845ddd9c812
TheRusmith <mpiscitelli98@gmail.com>
2022-02-16 09:23:35 +0100

    Initial Commit

---
+++ Autism/AutismApp.swift
@@ -0,0 +1,20 @@
+//
+//  AutismApp.swift
+//  Autism
+//
+//  Created by Mariano Piscitelli on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AutismApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
