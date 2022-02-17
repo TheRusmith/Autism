5ad5fdc06e837ae09d8dc4f0684b25077824b9af
TheRusmith <mpiscitelli98@gmail.com>
2022-02-17 09:49:59 +0100

    Some change on NavigationView

---
+++ Autism/NavigationView.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationView.swift
+//  Autism
+//
+//  Created by Mariano Piscitelli on 17/02/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Navigation: View {
+      
+    
+    @SceneStorage("activeTab") private var activeTab = Tab.home
+    
+    enum Tab: Int {
+        case home, buy, ticket
+    }
+    
+    var body: some View {
+        TabView(selection: $activeTab) {
+            HomeView()
+                .tabItem{
+                    Label("Home", systemImage: "house.fill")
+                }
+            FavView()
+                .tabItem{
+                    Label ("Favorites", systemImage: "star.fill")
+                }
+            CreateView()
+                .tabItem{
+                    Label("Create", systemImage: "pencil.circle.fill")
+            }
+        }
+    }
+}
+
+struct Navigation_Previews: PreviewProvider {
+    static var previews: some View {
+        Navigation()
+    }
+}
+
