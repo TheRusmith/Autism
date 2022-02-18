c96dda450885181544e02cf8fb01e21af9eb436a
TheRusmith <mpiscitelli98@gmail.com>
2022-02-18 12:27:31 +0100

    general commit

--- Autism/SmallcardImage.swift
+++ Autism/SmallcardImage.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SmallcardImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("fox")
+            .resizable()
+            .frame(width: 60, height: 60)
+            .border(<#T##content: ShapeStyle##ShapeStyle#>)
     }
 }
 
