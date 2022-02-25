3994683a6cdbfbcbe2701e6c8ec966385afbb6ea
TheRusmith <mpiscitelli98@gmail.com>
2022-02-25 11:42:32 +0100

    Feel view update

--- Autism/Sub-views/FeelView.swift
+++ Autism/Sub-views/FeelView.swift
@@ -4,44 +4,6 @@
 //
 //  Created by Mariano Piscitelli on 17/02/22.
 
-//import SwiftUI
-//
-//struct FeelView: View {
-//    @EnvironmentObject  var userStore: UserStore
-//    let speechservice = SpeechService()
-//
-//    var body: some View {
-//        VStack{
-//            BarView()
-//            HStack{
-//                NavigationView {
-//                    ScrollView(.vertical) {
-//                        VStack(alignment: .center, spacing: 25) {
-//                            ForEach(UserStore().wants,id: \.self) { want in
-//                                NavigationLink(destination: HomeView()) {
-//                                    CardModel(title: want.title,image: want.imageName)
-//                                }
-//                            }
-//                        }
-//                    }
-//                    .navigationBarHidden(true)
-//                }
-//            }
-//        }
-//            .onAppear {
-//                speechservice.say("Mi sento ")
-//            }
-//            .ignoresSafeArea()
-//            .navigationBarHidden(true)
-//    }
-//}
-//struct FeelView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        FeelView()
-//            .environmentObject(UserStore())
-//    }
-//}
-
 import SwiftUI
 struct FeelView: View {
     @EnvironmentObject  var userStore: UserStore
@@ -51,7 +13,7 @@ struct FeelView: View {
             BarView()
             NavigationView{
                 ScrollView(.vertical){
-                    LazyVStack(alignment: . center, spacing: 30) {
+                    VStack(alignment: . center, spacing: 30) {
                         
                         ForEach(UserStore().wants,id: \.self) { want in
                             NavigationLink(destination: HomeView()) {
