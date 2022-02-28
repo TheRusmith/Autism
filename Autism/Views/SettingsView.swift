f61a542bc8ff5daa6ca74e5c6f3fb81048a00889
TheRusmith <mpiscitelli98@gmail.com>
2022-02-28 11:25:29 +0100

    Settings view created

---
+++ Autism/Views/SettingsView.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsView.swift
+//  Autism
+//
+//  Created by Mariano Piscitelli on 28/02/22.
+//
+
+import SwiftUI
+
+struct SettingsView : View {
+    
+   
+    var body: some View {
+        
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
