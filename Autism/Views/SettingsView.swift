aa48b4f02b6570486d5d7a7ade69151edf1700aa
TheRusmith <mpiscitelli98@gmail.com>
2022-02-28 13:01:17 +0100

    Update

--- Autism/Views/SettingsView.swift
+++ Autism/Views/SettingsView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct SettingsView : View {
+    @State private var preselectedIndex = 0
+    @State private var voice = ["Male", "Female"]
     @State var username: String = ""
     let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+    var frameworks = ["English", "Italian", "Chinese", "Spanish"]
+    @State private var selectedFrameworkIndex = 0
     var body: some View {
         
         VStack{
+            
+//            Rectangle()
+//                    .frame(width: 20, height: 30)
+//                    .opacity(0)
             
             Image("fox")
                 .resizable()
@@ -40,16 +48,51 @@ struct SettingsView : View {
                         }
                     }
                 }
-        
+            
+        Rectangle()
+                .frame(width: 20, height: 30)
+                .opacity(0)
+            
         TextField("Nickname", text: $username)
+                .frame(width: 300, height: 20)
             .padding()
             .background(lightGreyColor)
-            .cornerRadius(5.0)
+            .cornerRadius(15)
             .padding(.bottom, 20)
             
+            Rectangle()
+                    .frame(width: 20, height: 30)
+                    .opacity(0)
             
+            Text("Voice")
+                .fontWeight(.bold)
+                   .font(.title)
+                   .foregroundColor(.orange)
             
-            Spacer()
+            Picker("City", selection: $preselectedIndex) {
+                            ForEach(0..<voice.count) { index in
+                                Text(voice[index])
+                                    .tag(index)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+            
+            Rectangle()
+                    .frame(width: 20, height: 30)
+                    .opacity(0)
+            
+            Text("Language")
+                .fontWeight(.bold)
+                   .font(.title)
+                   .foregroundColor(.orange)
+            
+            Picker(selection: $selectedFrameworkIndex, label: Text("")) {
+                        ForEach(0 ..< frameworks.count) {
+                           Text(self.frameworks[$0])
+                        }
+                     }
+            .pickerStyle(SegmentedPickerStyle())
+//            Spacer()
         }
     }
 }
