74dc788c646ec74816e19152cb39d0dd7b199cac
francescviola <92516495+francescviola@users.noreply.github.com>
2022-03-04 11:14:59 +0100

    Solved Text-to-speech problems

--- Autism/Sub-views/FinalView.swift
+++ Autism/Sub-views/FinalView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct FinalView: View {
     @State var messagesFinal = [String]()
     var body: some View {
+        let speechservice = SpeechService()
         VStack{
             HStack {
                 BarView(messages: messagesFinal)
+                    .onAppear{
+                        speechservice.say(messagesFinal.last!)
+                    }
     //            Text("ViewFinale")
             }
             HStack {
